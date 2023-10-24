b83d1a8be0881a99dfcc50a1ffcab2b30d1205f7
Aurélien GEORGES <aurelien.georges30@gmail.com>
2023-10-24 14:01:05 +0200

    fix: :sparkles: fix merge

--- api/script.sh
+++ api/script.sh
@@ -1,6 +1,2 @@
 symfony composer install
-<<<<<<< HEAD
 symfony server:start --no-tls
-=======
-symfony server:start --no-tls
->>>>>>> 9b41cdca7a737590519aee0915cf4010395768b3
--- client/script.sh
+++ client/script.sh
@@ -1,6 +1,2 @@
 npm install
-<<<<<<< HEAD
 npm start
-=======
-npm start
->>>>>>> 9b41cdca7a737590519aee0915cf4010395768b3
