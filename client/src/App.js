66277f1ec49c38b918d137758b765fc1cbbedcb3
Aurélien GEORGES <aurelien.georges30@gmail.com>
2023-10-25 17:08:01 +0200

    feat: :sparkles: beginning fixing symfony:requirements

--- client/src/App.js
+++ client/src/App.js
@@ -23,7 +23,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <button onClick={callApi}>Click Me!!</button>
+        <button onClick={callApi}>Click Me!</button>
         {loading ? (
           <p>Loading...</p> // Display the loading indicator
         ) : (
