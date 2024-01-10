32b4b592e058e67611a0b41983d5fb06c5b1501a
Aurélien GEORGES <aurelien.georges30@gmail.com>
2024-01-10 12:31:10 +0100

    feat: :sparkles: add factory to make better tests

--- api/tests/ItemFonctionnalTest.php
+++ api/tests/ItemFonctionnalTest.php
@@ -2,28 +2,39 @@
 
 namespace App\Tests;
 
+use App\Entity\Item;
+use App\Factory\ItemFactory;
+use Zenstruck\Foundry\Test\Factories;
+use Zenstruck\Foundry\Test\ResetDatabase;
 use ApiPlatform\Symfony\Bundle\Test\ApiTestCase;
 
 class ItemFonctionnalTest extends ApiTestCase
 {
+    use ResetDatabase, Factories;
+
     public function testGetItems()
     {
+        ItemFactory::createMany(10);
         $client = static::createClient();
         $client->request(
             'GET',
             '/api/items'
         );
         $this->assertResponseIsSuccessful();
+        $this->assertResponseHeaderSame('content-type', 'application/ld+json; charset=utf-8');
     }
 
     public function testGetItem()
     {
+        ItemFactory::createOne(['content' => 'hello world!']);
         $client = static::createClient();
+        $iri = $this->findIriBy(Item::class, ['content' => 'hello world!']);
         $client->request(
             'GET',
-            '/api/items/25'
+            $iri
         );
         $this->assertResponseIsSuccessful();
+        $this->assertResponseHeaderSame('content-type', 'application/ld+json; charset=utf-8');
     }
 
     public function testCreateItem()
@@ -35,36 +46,49 @@ class ItemFonctionnalTest extends ApiTestCase
             ['json' => ['content' => 'mon item']]
         );
         $this->assertResponseIsSuccessful();
+        $this->assertResponseHeaderSame('content-type', 'application/ld+json; charset=utf-8');
     }
 
     public function testUpdateItem()
     {
+        ItemFactory::createOne(['content' => 'little hello world!']);
         $client = static::createClient();
+        $iri = $this->findIriBy(Item::class, ['content' => 'little hello world!']);
         $client->request(
             'PUT',
-            '/api/items/45',
-            ['json' => ['content' => 'mon item update']]
+            $iri,
+            ['json' => [
+                'content' => 'big hello world!',
+            ]]
         );
         $this->assertResponseIsSuccessful();
+        $this->assertResponseHeaderSame('content-type', 'application/ld+json; charset=utf-8');
     }
 
     public function testPatchItem()
     {
+        ItemFactory::createOne(['content' => 'ha hello world!']);
         $client = static::createClient();
+        $iri = $this->findIriBy(Item::class, ['content' => 'ha hello world!']);
         $client->request(
             'PATCH',
-            '/api/items/15',
+            $iri,
             ['headers' => [
-                'Content-Type' => 'application/merge-patch+json',
-            ], 'json' => ['content' => 'mon item patched']]
+                'Content-Type' => 'application/merge-patch+json; charset=utf-8',
+            ], 'json' => ['content' => 'ho hello world']]
         );
         $this->assertResponseIsSuccessful();
     }
 
     public function testDeleteItem()
     {
+        ItemFactory::createOne(['content' => 'min hello world!']);
         $client = self::createClient();
-        $client->request('DELETE', '/api/items/9');
-        $this->assertResponseIsSuccessful(204);
+        $iri = $this->findIriBy(Item::class, ['content' => 'min hello world!']);
+        $client->request('DELETE', $iri);
+        $this->assertResponseStatusCodeSame(204);
+        $this->assertNull(
+            static::getContainer()->get('doctrine')->getRepository(Item::class)->findOneBy(['content' => 'min hello world!'])
+        );
     }
 }
