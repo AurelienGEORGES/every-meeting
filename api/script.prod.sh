6dc21facfe21ca4d58be19a2c72ffaeda280e2e0
AurelienGEORGES <aurelien.georges30@gmail.com>
2024-10-31 14:50:19 +0100

    feat: :sparkles: error prod vers portainer

--- api/script.prod.sh
+++ api/script.prod.sh
@@ -1,1 +1,4 @@
-php  bin/console lexik:jwt:generate-keypair --overwrite --no-interaction
+php bin/console doctrine:database:create --no-interaction --if-not-exists
+php bin/console doctrine:migrations:migrate --no-interaction
+php bin/console lexik:jwt:generate-keypair --overwrite --no-interaction 
+apache2-foreground
